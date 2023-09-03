c84182dbabcbce1ab687cad38a526a3edb9b6ab1
Abdi <abdullahia0215@gmail.com>
2023-09-03 15:38:04 -0500

    added to script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,35 @@
-console.log("Hello World");
+document.addEventListener('DOMContentLoaded', function() {
+  const employeeForm = document.getElementById('employee-form');
+  const employeesList = document.getElementById('employees-list');
+  const totalMonthlyCost = document.getElementById('total-monthly-cost');
 
-$(document).ready(
-    console.log("jQuery is ready")
-)
+  let monthlyCost = 0;
+
+  employeeForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const firstName = document.getElementById('first-name').value;
+    const lastName = document.getElementById('last-name').value;
+    const idNumber = document.getElementById('id-number').value;
+    const jobTitle = document.getElementById('job-title').value;
+    const annualSalary = parseInt(document.getElementById('annual-salary').value);
+
+    const employee = {
+      firstName,
+      lastName,
+      idNumber,
+      jobTitle,
+      annualSalary
+    };
+
+    const employeeEntry = document.createElement('div');
+    employeeEntry.innerHTML = `
+      <p>${firstName} ${lastName}</p>
+      <p>ID: ${idNumber}</p>
+      <p>Job Title: ${jobTitle}</p>
+      <p>Annual Salary: $${annualSalary}</p>
+      <button class="delete-btn">Delete</button>
+    `;
+
+  });
+});
